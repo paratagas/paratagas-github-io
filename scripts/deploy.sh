afedfe183221ee0ab0d53c8ad2eb1358e4682fc9
Yauheni Svirydzenka <partagas@mail.ru>
2018-06-29 23:09:47 +0300

    Fix config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,6 @@ echo 'Starting deploy...'
 rm -rf ../paratagas.github.io/*
 cp -r ./build/. ../paratagas.github.io
 cp ./README.md ../paratagas.github.io/README.md
+cp ./google6791230e8966e716.html ../paratagas.github.io/home/google6791230e8966e716.html
 
 echo 'Deploy finished'
